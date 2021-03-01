7e84fb430f8d56bfa10bfd95249833a979af65e2
Justin Ochoa <just.och92@gmail.com>
2021-03-01 07:53:10 -0500

    counter app

---
+++ counter-app/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import Counters from "./component/Counters";
+
+function App() {
+  return (
+    <div className="App">
+      <Counters />
+    </div>
+  );
+}
+
+export default App;
